33ca8b83a59932036dc217ffcb4299cf9989efb0
Ming <zhaomingming455@163.com>
2022-02-13 17:01:11 +0800

    feat: binaryTree

---
+++ src/binaryTree/binaryTree.h
@@ -0,0 +1,36 @@
+
+#include<iostream>
+#include<cstring>
+#include<cassert>
+#include<queue>
+
+using namespace std;
+
+typedef char DataType;
+
+struct TreeNode {
+
+    DataType data;    /* node data */
+
+    TreeNode *leftPtr;   /* pointer to left subtree */
+
+    TreeNode *rightPtr;  /* pointer to right subtree */
+};
+
+typedef struct TreeNode TreeNode;
+
+typedef TreeNode * pTreeNode;
+
+void CreateBinTree(pTreeNode *Tree);//创建二叉树
+
+void InitTreeNode(pTreeNode *Tree);//初始化
+
+void PreOrderPrint(pTreeNode Tree);//先序遍历
+
+void MidOrderPrint(pTreeNode Tree);//中序遍历
+
+void PostOrderPrint(pTreeNode Tree);//后续遍历
+
+void FloorPrint(pTreeNode Tree);//层序遍历
+
+void FloorPrint_QUEUE(pTreeNode &Tree);//层序遍历_队列实现
---
+++ src/binaryTree/testBinaryTree.cpp
@@ -0,0 +1,31 @@
+#include"binaryTree.h"
+
+void test()
+{
+    cout << "test\n";
+    pTreeNode T;
+    InitTreeNode(&T);
+
+    CreateBinTree(&T);  //创建一个二叉树
+
+    cout << "前序遍历:" << endl;
+    PreOrderPrint(T);    //前序遍历
+
+    cout << "\n中序遍历:" << endl;
+    MidOrderPrint(T);   //中序遍历
+
+    cout << "\n后序遍历:" << endl;
+    PostOrderPrint(T);   //后续遍历
+
+    cout << "\n层序遍历:" << endl;
+    FloorPrint(T);
+
+    cout << "\n层序遍历——Queue:" << endl;
+    FloorPrint_QUEUE(T);
+}
+
+int main(void)
+{
+    test();
+    return 0;
+}
